53f26cb32b34ef1353b39c32f46a064b10233201
Josiah Dahl <josiah.dahl@gmail.com>
2018-11-20 12:55:55 -0800

    fix: Fix quota event return types

--- src/Events/SmartApp/QuotaUpdated.php
+++ src/Events/SmartApp/QuotaUpdated.php
@@ -6,28 +6,12 @@ use LHP\Services\Events\Contracts\ServiceEvent;
 
 class QuotaUpdated implements ServiceEvent
 {
-    /**
-     * @var
-     */
-    private $serviceUserId;
-
-    /**
-     * StandardAccountCreated constructor.
-     *
-     * @param $serviceUserId
-     */
-    public function __construct($serviceUserId)
-    {
-        $this->serviceUserId = $serviceUserId;
-    }
 
     /**
      * @return array
      */
     public function toArray(): array
     {
-        return [
-            'serviceUserId' => $this->serviceUserId,
-        ];
+        return [];
     }
 }
