305e0d8e790ebde004dfc92e12dadc060274e807
Justin Zablocki <zablockijj@gmail.com>
2018-12-13 13:00:30 -0800

    Change variable passed to RegisterConsumer to accountId

--- src/Commands/SSO/RegisterConsumer.php
+++ src/Commands/SSO/RegisterConsumer.php
@@ -15,7 +15,7 @@ class RegisterConsumer extends ServiceCommand
     /**
      * @var int
      */
-    private $serviceId;
+    private $accountId;
 
     /**
      * @var int|null
@@ -31,14 +31,14 @@ class RegisterConsumer extends ServiceCommand
      * RegisterConsumer constructor.
      *
      * @param string   $email
-     * @param int      $serviceId
+     * @param int      $accountId
      * @param int|null $loanOfficerId
      * @param int|null $branchId
      */
-    public function __construct(string $email, int $serviceId, ?int $loanOfficerId = null, ?int $branchId = null)
+    public function __construct(string $email, int $accountId, ?int $loanOfficerId = null, ?int $branchId = null)
     {
         $this->email         = $email;
-        $this->serviceId     = $serviceId;
+        $this->accountId     = $accountId;
         $this->loanOfficerId = $loanOfficerId;
         $this->branchId      = $branchId;
     }
@@ -52,15 +52,13 @@ class RegisterConsumer extends ServiceCommand
     }
 
     /**
-     * The payload of the command
-     *
      * @return array
      */
     public function payload(): array
     {
         return [
             'email'         => $this->email,
-            'serviceId'     => $this->serviceId,
+            'accountId'     => $this->accountId,
             'loanOfficerId' => $this->loanOfficerId,
             'branchId'      => $this->branchId,
         ];
