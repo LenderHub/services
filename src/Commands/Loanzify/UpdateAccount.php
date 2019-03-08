8de72bc8e0d984150c4acbf140efd45efc6c4ae8
Marcos Schlucat Grimm <marcosgrimm@gmail.com>
2019-03-08 16:10:26 -0300

    SSO-144 adding commands to update user on loanzify

---
+++ src/Commands/Loanzify/UpdateAccount.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace LHP\Services\Commands\Loanzify;
+
+use LHP\Services\Commands\ServiceCommand;
+use LHP\Services\Events\Loanzify\AccountUpdated;
+
+class UpdateAccount extends ServiceCommand
+{
+    /**
+     * @var array
+     */
+    private $userId;
+
+    /**
+     * @var int
+     */
+    private $serviceUserId;
+
+    /**
+     * UpdateAccount constructor.
+     *
+     * @param int $serviceUserId
+     */
+    public function __construct(int $userId, int $serviceUserId)
+    {
+        $this->serviceUserId    = $serviceUserId;
+        $this->userId = $userId;
+    }
+
+    /**
+     * The event class the command expects to receive on successful execution
+     *
+     * @return string
+     */
+    public function expects(): string
+    {
+        return AccountUpdated::class;
+    }
+
+    /**
+     * The payload of the command
+     *
+     * @return array
+     */
+    public function payload(): array
+    {
+        return [
+            'serviceUserId' => $this->serviceUserId,
+            'userId' => $this->userId,
+        ];
+    }
+}
