ff0dedbf7e2aaa05c81b876e55fd548c83663e3e
Mathias Grimm <mathiasgrimm@gmail.com>
2019-05-15 17:47:03 -0300

    PS-134 initial commit

---
+++ src/Commands/POS/CreateStaff.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace LHP\Services\Commands\POS;
+
+use LHP\Services\Commands\ServiceCommand;
+use LHP\Services\Events\POS\LoanOfficerCreated;
+
+class CreateStaff extends ServiceCommand
+{
+    private $ssoUserId;
+    private $ssoParentUserId;
+    private $ssoBranchId;
+    
+    public function __construct(int $ssoUserId, int $ssoParentUserId, ?int $ssoBranchId)
+    {
+        $this->ssoUserId = $ssoUserId;
+        $this->ssoParentUserId = $ssoParentUserId;
+        $this->ssoBranchId = $ssoBranchId;
+    }
+
+    public function expects(): string
+    {
+        return LoanOfficerCreated::class;
+    }
+
+    public function payload(): array
+    {
+        return [
+            'ssoUserId' => $this->ssoUserId,
+            'ssoParentUserId' => $this->ssoParentUserId,
+            'ssoBranchId' => $this->ssoBranchId,
+        ];
+    }
+}
---
+++ src/Events/POS/StaffCreated.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LHP\Services\Events\POS;
+
+use LHP\Services\Events\Contracts\ServiceEvent;
+
+class StaffCreated implements ServiceEvent
+{
+    private $posAccountId;
+    private $posAccountUserId;
+    
+    public function __construct(int $posAccountId, int $posAccountUserId)
+    {
+        $this->posAccountId = $posAccountId;
+        $this->posAccountUserId = $posAccountUserId;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'posAccountId' => $this->posAccountId,
+            'posAccountUserId' => $this->posAccountUserId,
+        ];
+    }
+}
