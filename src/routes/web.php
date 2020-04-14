2b01c83a707080a29d838addff92097087ac348b
Titas Gailius <titas.gailius@gmail.com>
2020-04-14 16:07:08 +0200

    Cacheable routing

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,51 +1,5 @@
 <?php
 
-Route::get('/api/lenderhub/services/tokens', function () {
-    return [
-        'sso' => Firebase\JWT\JWT::encode(
-            [
-                'iat'   => time(),
-                'exp'   => time() + 300,
-            ],
-            //env('SECRET_KEY'),
-            config('lhp-services.sso.secret'),
-            'HS256'
-        ),
-        'lhp' => Firebase\JWT\JWT::encode(
-            [
-                'iat'   => time(),
-                'exp'   => time() + 300,
-            ],
-            //env('SECRET_KEY'),
-            config('lhp-services.lhp.secret'),
-            'HS256'
-        ),
-        'loanzify' => Firebase\JWT\JWT::encode(
-            [
-                'iat'   => time(),
-                'exp'   => time() + 300,
-            ],
-            //env('SECRET_KEY'),
-            config('lhp-services.loanzify.secret'),
-            'HS256'
-        ),
-        'loanzifyV3' => Firebase\JWT\JWT::encode(
-            [
-                'iat'   => time(),
-                'exp'   => time() + 300,
-            ],
-            //env('SECRET_KEY'),
-            config('lhp-services.loanzifyV3.secret'),
-            'HS256'
-        ),
-        'smartapp' => Firebase\JWT\JWT::encode(
-            [
-                'iat'   => time(),
-                'exp'   => time() + 300,
-            ],
-            //env('SECRET_KEY'),
-            config('lhp-services.smartapp.secret'),
-            'HS256'
-        ),
-    ];
+Route::namespace('LHP\Services\Http\Controllers')->group(function () {
+    Route::get('api/lenderhub/services/tokens', 'TokenController');
 });
